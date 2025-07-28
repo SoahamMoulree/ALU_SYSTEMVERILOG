14b282ad158df8ff1c1add36063e50194e5f7c03
SoahamMoulree <soaham.moulree@gmail.com>
2025-07-28 22:08:25 +0530

    Create alu_pkg.sv

---
+++ ALU_CODE_TESTBENCH/alu_pkg.sv
@@ -0,0 +1,11 @@
+package alu_pkg;
+  `include "transaction.sv"
+  `include "generator.sv"
+  `include "driver.sv"
+  `include "reference_model.sv"
+  `include "monitor.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+  `include "test.sv"
+endpackage
+~
